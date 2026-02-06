efa696d5ed7ffc78700a7979219c621336e0cf3f
Paul M. Jones <pmjones@pmjones.io>
2026-02-06 08:58:57 -0600

    use stardoc

--- config/README.config.php
+++ config/README.config.php
@@ -1,6 +1,8 @@
 <?php return [
+    'namespace' => 'FrontInterop\\Interface\\',
+    'directory' => dirname(__DIR__) . '/src',
     'template' => dirname(__DIR__) . '/resources/README.tpl.md',
-    'files' => [
-        dirname(__DIR__) . "/src/FrontController.php",
+    'interfaces' => [
+        'FrontController',
     ],
 ];
